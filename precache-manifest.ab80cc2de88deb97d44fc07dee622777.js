a2cd61ef4d87f25bbe12795f5a4e420df1f46c33
Marco Rettore <marcorettoremattes@gmail.com>
2022-03-21 16:05:16 -0300

    Updates

---
+++ precache-manifest.ab80cc2de88deb97d44fc07dee622777.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "32d077d5f46e035ce61a",
+    "url": "/todo-app/static/css/main.b3195ec5.chunk.css"
+  },
+  {
+    "revision": "32d077d5f46e035ce61a",
+    "url": "/todo-app/static/js/main.32d077d5.chunk.js"
+  },
+  {
+    "revision": "2e2c972344455e786880",
+    "url": "/todo-app/static/js/1.2e2c9723.chunk.js"
+  },
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "5e6d8bbcf168265a84edc138d8bd29b0",
+    "url": "/todo-app/static/media/logo-img.5e6d8bbc.png"
+  },
+  {
+    "revision": "c710b0d2609482f0058398cb1b1cd7cf",
+    "url": "/todo-app/static/media/undraw_add_notes_re_ln36.c710b0d2.svg"
+  },
+  {
+    "revision": "67f7594bb698fdada3e62ca18d3a6dfa",
+    "url": "/todo-app/static/media/undraw_page_not_found_re_e9o6.67f7594b.svg"
+  },
+  {
+    "revision": "9d6328592b399acc5d80c9593ddc2b3f",
+    "url": "/todo-app/index.html"
+  }
+];
